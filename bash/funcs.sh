3fff5410290766d768faf8bc9654dc054df2372a
Miles Z. Sterrett <miles.sterrett@gmail.com>
2009-06-05 10:01:38 -0400

    Add nospace function

--- bash/funcs.sh
+++ bash/funcs.sh
@@ -11,42 +11,48 @@ function fe() { find . -name '*'$1'*' -exec $2 {} \; ; }
 # Find a file ending in $2 and search for string $1 in it
 function fstr() # find a string in a set of files
 {
-    if [ $# -ne 2 ]; then
-        echo "Usage: fstr \"pattern\" [files] "
-        return;
-    fi
-    SMSO=$(tput smso)
-    RMSO=$(tput rmso)
-    find . -type f -name "*${2}" -print | xargs grep -sin "$1" | \
-    sed "s/$1/$SMSO$1$RMSO/gI"
+  if [ $# -ne 2 ]; then
+    echo "Usage: fstr \"pattern\" [files] "
+    return;
+  fi
+  SMSO=$(tput smso)
+  RMSO=$(tput rmso)
+  find . -type f -name "*${2}" -print | xargs grep -sin "$1" | \
+  sed "s/$1/$SMSO$1$RMSO/gI"
 }
 
 # Move filenames to lowercase
 function lowercase()
 {
-    for file ; do
-        filename=${file##*/}
-        case "$filename" in
-        */*) dirname==${file%/*} ;;
-        *) dirname=.;;
-        esac
-        nf=$(echo $filename | tr A-Z a-z)
-        newname="${dirname}/${nf}"
-        if [ "$nf" != "$filename" ]; then
-            mv "$file" "$newname"
-            echo "lowercase: $file --> $newname"
-        else
-            echo "lowercase: $file not changed."
-        fi
-    done
+  for file ; do
+    filename=${file##*/}
+    case "$filename" in
+      */*) dirname==${file%/*} ;;
+      *) dirname=.;;
+    esac
+    nf=$(echo $filename | tr A-Z a-z)
+    newname="${dirname}/${nf}"
+    if [ "$nf" != "$filename" ]; then
+      mv "$file" "$newname"
+    else
+      echo "lowercase: $file not changed."
+    fi
+  done
 }
+
+# Swap spaces for underscores in file names
+function nospace()
+{
+  mv "$1" "`echo $1 | tr ' ' '_'`"
+}
+
 
 # Swap file $1 with $2
 function swap() {
-  local TMPFILE=tmp.$$
-  mv $1 $TMPFILE
-  mv $2 $1
-  mv $TMPFILE $2
+local TMPFILE=tmp.$$
+mv $1 $TMPFILE
+mv $2 $1
+mv $TMPFILE $2
 }
 
 # Process/System related functions
@@ -60,21 +66,21 @@ function pp() { my_ps f | awk '!/awk/ && $0~var' var=${1:-".*"} ; }
 # get IP address
 function my_ip()
 {
-    MY_IP=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 } ' | sed -e s/addr://)
-    MY_ISP=$(/sbin/ifconfig eth0 | awk '/P-t-P/ { print $3 } ' | sed -e s/P-t-P://)
+  MY_IP=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 } ' | sed -e s/addr://)
+  MY_ISP=$(/sbin/ifconfig eth0 | awk '/P-t-P/ { print $3 } ' | sed -e s/P-t-P://)
 }
 
 # get current host related info
 function ii()
 {
-    echo -e "\nYou are logged on ${RED}$HOST"
-    echo -e "\nAdditionnal information:$NC " ; uname -a
-    echo -e "\n${RED}Users logged on:$NC " ; w -h
-    echo -e "\n${RED}Current date :$NC " ; date
-    echo -e "\n${RED}Machine stats :$NC " ; uptime
-    echo -e "\n${RED}Memory stats :$NC " ; free
-    my_ip 2>&- ;
-    echo -e "\n${RED}Local IP Address :$NC" ; echo ${MY_IP:-"Not connected"}
-    echo -e "\n${RED}ISP Address :$NC" ; echo ${MY_ISP:-"Not connected"}
-    echo
+  echo -e "\nYou are logged on ${RED}$HOST"
+  echo -e "\nAdditionnal information:$NC " ; uname -a
+  echo -e "\n${RED}Users logged on:$NC " ; w -h
+  echo -e "\n${RED}Current date :$NC " ; date
+  echo -e "\n${RED}Machine stats :$NC " ; uptime
+  echo -e "\n${RED}Memory stats :$NC " ; free
+  my_ip 2>&- ;
+  echo -e "\n${RED}Local IP Address :$NC" ; echo ${MY_IP:-"Not connected"}
+  echo -e "\n${RED}ISP Address :$NC" ; echo ${MY_ISP:-"Not connected"}
+  echo
 }
