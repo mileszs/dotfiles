70d9265e26f19b3a2f724f19152bafa9da5d84af
Miles Z. Sterrett <miles.sterrett@gmail.com>
2010-04-04 16:56:51 -0400

    Work on myip, ii functions.
    Still not perfect, but I'm tired of messing with them

--- bash/funcs.sh
+++ bash/funcs.sh
@@ -64,24 +64,23 @@ function my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,bsdtime,command ; }
 function pp() { my_ps f | awk '!/awk/ && $0~var' var=${1:-".*"} ; }
 
 # get IP address
-function my_ip()
+function myip()
 {
-  MY_IP=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 } ' | sed -e s/addr://)
-  MY_ISP=$(/sbin/ifconfig eth0 | awk '/P-t-P/ { print $3 } ' | sed -e s/P-t-P://)
+  IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`;
+  echo $IP
 }
 
 # get current host related info
 function ii()
 {
-  echo -e "\nYou are logged on ${RED}$HOST"
+  echo -e "\nYou are logged on ${BLUE}$HOSTNAME${NC}"
   echo -e "\nAdditionnal information:$NC " ; uname -a
   echo -e "\n${RED}Users logged on:$NC " ; w -h
   echo -e "\n${RED}Current date :$NC " ; date
   echo -e "\n${RED}Machine stats :$NC " ; uptime
   echo -e "\n${RED}Memory stats :$NC " ; free
-  my_ip 2>&- ;
-  echo -e "\n${RED}Local IP Address :$NC" ; echo ${MY_IP:-"Not connected"}
-  echo -e "\n${RED}ISP Address :$NC" ; echo ${MY_ISP:-"Not connected"}
+  myip > /dev/null 2>&1
+  echo -e "\n${RED}Local IP Address :$NC" ; echo ${IP:-"Not connected"}
   echo
 }
 
